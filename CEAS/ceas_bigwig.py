76830520b69c00c718191f4146b7ac3b61cf9e42
Tao Liu (τν) <vladimir.liu@gmail.com>
2025-08-10 12:00:40 -0400

    refactor: move parsers to bin scripts

--- CEAS/ceas_bigwig.py
+++ CEAS/ceas_bigwig.py
@@ -42,7 +42,15 @@ except:
     sys.exit()
 
 from CEAS.bigwig_utils import open_bigwig
-from CEAS import ceas as ceas_parser
+import importlib.util
+import importlib.machinery
+from pathlib import Path
+
+_ceas_path = Path(__file__).resolve().parents[1] / "bin" / "ceas"
+_loader = importlib.machinery.SourceFileLoader("ceas_cli", str(_ceas_path))
+_spec = importlib.util.spec_from_loader(_loader.name, _loader)
+ceas_parser = importlib.util.module_from_spec(_spec)
+_loader.exec_module(ceas_parser)
 
 # ------------------------------------
 # constants
